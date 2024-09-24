7c89e1b3f60ef04247e7be3d4df68dc14ea9a5be
xloc <xloc.cc@outlook.com>
2024-09-24 01:43:11 -0700

    project scaffold

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="w-screen h-screen flex justify-center items-center text-3xl text-purple-800">
+    Hello Graph Editor!
+  </div>
+</template>
+
+<style scoped></style>
